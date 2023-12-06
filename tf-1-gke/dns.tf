be6e9956b55364a830abae0a63fff8ee2695489b
Johannes Batzill <johannes.batzill@harness.io>
2023-12-06 09:12:04 -0800

    Update dns.tf

--- tf-1-gke/dns.tf
+++ tf-1-gke/dns.tf
@@ -1,5 +1,5 @@
 resource "google_dns_managed_zone" "zone" {
-  name        = trim(replace(local.dns_zone, ".", "-"), "-")
-  dns_name    = local.dns_zone
+  name        = replace(local.dns_zone, ".", "-")
+  dns_name    = ${local.dns_zone}."
   description = "Test zone for GKE daytona workspaces"
 }
