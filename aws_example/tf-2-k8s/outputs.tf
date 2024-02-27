57b197deea4d64ba051e144c81012cafb6107fce
Zoran Zorica <zoran.zorica@gmail.com>
2024-02-27 11:16:55 +0100

    chore(aws): output keycloak credentials

---
+++ aws_example/tf-2-k8s/outputs.tf
@@ -0,0 +1,26 @@
+output "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  value       = local.cluster_name
+}
+
+output "daytona_url" {
+  description = "Daytona dashboard URL"
+  value       = "https://${local.dns_zone}"
+}
+
+output "keycloak_url" {
+  description = "Keycloak URL"
+  value       = "https://id.${local.dns_zone}"
+}
+
+data "kubernetes_secret" "keycloak_admin" {
+  metadata {
+    name      = "watkins-watkins-keycloak"
+    namespace = kubernetes_namespace.watkins.metadata[0].name
+  }
+}
+
+output "keycloak_admin_password" {
+  description = "Keycloak user/password"
+  value       = "admin / ${nonsensitive(data.kubernetes_secret.keycloak_admin.data["admin-password"])}"
+}
